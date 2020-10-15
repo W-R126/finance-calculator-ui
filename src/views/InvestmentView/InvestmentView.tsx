3ea92e3e5cfead7e6b3f28dc4702de0fd262cd0c
rz-roy <rz.royzhang@gmail.com>
2020-10-15 16:14:49 +0200

    fix temporary comment being visible

--- src/views/InvestmentView/InvestmentView.tsx
+++ src/views/InvestmentView/InvestmentView.tsx
@@ -40,7 +40,7 @@ export const InvestmentView: React.FC = () => {
 
     return (
         <>
-            {false && <NavBar />} // This is so that the pipeline will not complain about unused component. It will be activated for auth
+            {false && <NavBar /> /* This is so that the pipeline will not complain about unused component. It will be activated for auth */}
             when it's done.
             <Container maxWidth="sm">
                 {isFetching && (
