c0cf3e039c868dd2bbdd7e185060b315f2b9a0d1
rz-roy <rz.royzhang@gmail.com>
2020-10-15 16:35:06 +0200

    Add alert

--- src/views/InvestmentView/InvestmentView.tsx
+++ src/views/InvestmentView/InvestmentView.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, CircularProgress, Container} from '@material-ui/core';
+import {Box, Button, CircularProgress, Container, Snackbar} from '@material-ui/core';
 import React, {useEffect, useState} from 'react';
 import {InvestmentInfo} from './InvestmentInfo';
 import {NavBar} from '../../components/NavBar/NavBar';
@@ -8,6 +8,7 @@ import {useInvestmentsAPI} from '../../hooks/useInvestmentsAPI';
 import {Separator} from '../../components/Separator/Separator';
 import {InvestmentParameters} from '../../api/investmentsAPI.types';
 import {inYears} from '../../helpers/inYears';
+import {Alert} from '@material-ui/lab';
 
 const mockedParameters: InvestmentParameters = {
     initialDepositValue: 1800,
@@ -38,8 +39,27 @@ export const InvestmentView: React.FC = () => {
         });
     };
 
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
     return (
         <>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    Saved to portfolio!
+                </Alert>
+            </Snackbar>
             {false && (
                 <NavBar />
             ) /* This is so that the pipeline will not complain about unused component. It will be activated for auth when it's done.*/}
@@ -61,6 +81,17 @@ export const InvestmentView: React.FC = () => {
                         onClick={handleSubmit}
                     >
                         Calculate
+                    </Button>
+
+                    <Button
+                        type={'submit'}
+                        disabled={isFetching}
+                        variant={'contained'}
+                        color={'primary'}
+                        style={{borderRadius: 25, marginLeft: 32}}
+                        onClick={handleClick}
+                    >
+                        Save to portfolio
                     </Button>
                 </Box>
             </Container>
