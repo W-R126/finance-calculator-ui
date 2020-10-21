1e8c8dfba1c6921a267814de289222caa0afcf22
rz-roy <rz.royzhang@gmail.com>
2020-10-21 14:22:18 +0200

    add login form

--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -1,18 +1,78 @@
 import React from 'react';
-import {Box, Container, Paper, Typography} from '@material-ui/core';
+import {Box, Button, Container, Paper, TextField, Typography} from '@material-ui/core';
+import {Formik, FormikHelpers} from 'formik';
+import {LoginFormData} from './LoginView.types';
 
 export const LoginView: React.FC = () => {
+    const handleOnSubmit = (values: LoginFormData, {setSubmitting}: FormikHelpers<LoginFormData>) => {
+        setTimeout(() => setSubmitting(false), 1000);
+    };
+
+    const validate = () => {};
+
     return (
         <Container maxWidth="sm">
-            <Box mt={5} textAlign="center">
+            <Box mt={3} textAlign="center">
                 <Paper>
-                    <Typography variant="h3">LOGIN</Typography>
+                    <Box p={2}>
+                        <Typography variant="h3">LOGIN</Typography>
+                    </Box>
                 </Paper>
             </Box>
             <Box mt={3}>
                 <Paper>
                     <Box p={3}>
-                        <Typography>LOGIN FORM GOES HERE</Typography>
+                        <Formik initialValues={{username: '', password: ''} as LoginFormData} validate={validate} onSubmit={handleOnSubmit}>
+                            {props => {
+                                const {values, touched, errors, isSubmitting, isValid, handleChange, handleBlur, handleSubmit} = props;
+
+                                return (
+                                    <form onSubmit={handleSubmit}>
+                                        <Box mt={3}>
+                                            <TextField
+                                                label="Username"
+                                                type="text"
+                                                name="username"
+                                                fullWidth
+                                                value={values.username}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                variant="outlined"
+                                                error={!!(errors.username && touched.username)}
+                                                helperText={errors.username && touched.username && errors.username}
+                                            />
+                                        </Box>
+
+                                        <Box mt={3}>
+                                            <TextField
+                                                label="Password"
+                                                type="password"
+                                                name="password"
+                                                fullWidth
+                                                value={values.password}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                variant="outlined"
+                                                error={!!(errors.password && touched.password)}
+                                                helperText={errors.password && touched.password && errors.password}
+                                            />
+                                        </Box>
+
+                                        <Box mt={3} display="flex" justifyContent="center">
+                                            <Button
+                                                size="large"
+                                                variant="contained"
+                                                color="primary"
+                                                type="submit"
+                                                disabled={isSubmitting || !isValid}
+                                            >
+                                                Log In
+                                            </Button>
+                                        </Box>
+                                    </form>
+                                );
+                            }}
+                        </Formik>
                     </Box>
                 </Paper>
             </Box>
