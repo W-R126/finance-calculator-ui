9b5fe6225f35262bbf34001abfd37ab08f2ee700
rz-roy <rz.royzhang@gmail.com>
2020-10-21 14:06:52 +0200

    add formik

--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import {Container} from '@material-ui/core';
+import {Box, Container, Paper, Typography} from '@material-ui/core';
 
 export const LoginView: React.FC = () => {
-    return <Container maxWidth="sm">LOGIN VIEW SAYS HELLO</Container>;
+    return (
+        <Container maxWidth="sm">
+            <Box mt={5} textAlign="center">
+                <Paper>
+                    <Typography variant="h3">LOGIN</Typography>
+                </Paper>
+            </Box>
+            <Box mt={3}>
+                <Paper>
+                    <Box p={3}>
+                        <Typography>LOGIN FORM GOES HERE</Typography>
+                    </Box>
+                </Paper>
+            </Box>
+        </Container>
+    );
 };
