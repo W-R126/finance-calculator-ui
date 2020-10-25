2e0688bcdd852d8bdd8098c014e7545886206312
rz-roy <rz.royzhang@gmail.com>
2020-10-25 15:05:52 +0100

    allow deleting portfolios

--- src/hooks/usePortfoliosAPI.ts
+++ src/hooks/usePortfoliosAPI.ts
@@ -1,8 +1,8 @@
 import {useEffect, useState} from 'react';
 import {Portfolio, PortfolioDetails} from '../api/portfoliosAPI.types';
-import {getPortfolioDetails, getPortfolios} from '../api/portfoliosAPI';
+import {deletePortfolio, getPortfolioDetails, getPortfolios} from '../api/portfoliosAPI';
 
-export function usePortfoliosAPI(): [Portfolio[], boolean, (id: number) => void, PortfolioDetails, boolean] {
+export function usePortfoliosAPI() {
     const [isFetchingPortfolios, setFetchingPortfolios] = useState(true);
     const [portfolios, setPortfolios] = useState<Portfolio[]>([]);
 
@@ -50,5 +50,31 @@ export function usePortfoliosAPI(): [Portfolio[], boolean, (id: number) => void,
         }
     };
 
-    return [portfolios, isFetchingPortfolios, fetchPortfolio, portfolio, isFetchingDetails];
+    const deleteCurrentPortfolio = () => {
+        setFetchingPortfolios(true);
+        setFetchingDetails(true);
+        deletePortfolio(portfolio.id).then(success => {
+            getPortfolios()
+                .then(portfolios => setPortfolios(portfolios))
+                .finally(() => {
+                    setFetchingPortfolios(false);
+                });
+            getPortfolioDetails(0)
+                .then(details => {
+                    details.id = 0;
+                    details.rateOfReturnPercentage = details.rateOfReturnPercentage ?? 0;
+                    setPortfolio(details);
+                })
+                .finally(() => setFetchingDetails(false));
+        });
+    };
+
+    return {
+        portfolios,
+        isFetchingPortfolios,
+        fetchPortfolio,
+        deleteCurrentPortfolio,
+        portfolio,
+        isFetchingDetails,
+    };
 }
