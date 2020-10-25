4e350bfde9adde6cf98eb184e5c8eca406f593de
rz-roy <rz.royzhang@gmail.com>
2020-10-25 15:33:37 +0000

    MET-89 userMenu implementation

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,24 @@
-import {AppBar, Box, Button, styled, Toolbar, Typography} from '@material-ui/core';
-import React from 'react';
+import {AppBar, Box, Button, FormControl, Menu, MenuItem, Select, styled, Toolbar, Typography} from '@material-ui/core';
+import React, {useState} from 'react';
 import {useUserState} from '../../contexts/authContext';
 import {Link, useLocation} from 'react-router-dom';
 import {routeToText} from './NavBar.helpers';
 import * as styles from './NavBar.styles';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import {UserMenu} from '../NavMenu/UserMenu';
 
 interface Props {}
 
 export const NavBar = (props: Props) => {
+    // hooks
     const authContext = useUserState();
-    const loginText = authContext.isAuth ? 'Hello ' + authContext.username : 'Login/Sign up';
     const location = useLocation();
+
+    const isAuth = authContext.isAuth;
+    const loginText = isAuth ? null : 'Login/Sign up';
     const nav_title = routeToText(location.pathname);
     const inPortfolio = nav_title === 'portfolio';
+
     const goBack = () => {
         return (
             <Link className={styles.Arrow} to={'/portfolios'}>
@@ -21,6 +26,19 @@ export const NavBar = (props: Props) => {
             </Link>
         );
     };
+
+    const userMenu = () => {
+        return <UserMenu username={authContext.username} />;
+    };
+
+    const loginButton = () => {
+        return (
+            <Link className={styles.Link} to={'/login'}>
+                <Button color="inherit">{loginText}</Button>
+            </Link>
+        );
+    };
+
     return (
         <AppBar position="static">
             <StyledToolbar>
@@ -28,11 +46,7 @@ export const NavBar = (props: Props) => {
                     {inPortfolio ? '' : goBack()}
                     <Typography className={inPortfolio ? styles.TitleNoArrow : styles.TitleArrow}>{nav_title}</Typography>
                 </Box>
-                <Box>
-                    <Link className={styles.Link} to={'/login'}>
-                        <Button color="inherit">{loginText}</Button>
-                    </Link>
-                </Box>
+                <Box>{isAuth ? userMenu() : loginButton()}</Box>
             </StyledToolbar>
         </AppBar>
     );
