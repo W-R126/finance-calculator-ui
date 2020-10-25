b31e100c0a84614b70de13a0b0223281a2bf7252
rz-roy <rz.royzhang@gmail.com>
2020-10-25 11:19:54 +0000

    MET-89 nav bar basic functionality

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,32 @@
 import {AppBar, Button, styled, Toolbar} from '@material-ui/core';
 import React from 'react';
+import {useUserState} from '../../contexts/authContext';
+import {Link, useLocation} from 'react-router-dom';
+import {routeToText} from './NavBar.helpers';
 
 interface Props {}
 
 export const NavBar = (props: Props) => {
+    const authContext = useUserState();
+    const loginText = authContext.isAuth ? 'Hello ' + authContext.username : 'Login/Sign up';
+    const location = useLocation();
+    const nav_title = routeToText(location.pathname);
+    const goBack = () => {
+        return (
+            <Link to={'/portfolios'}>
+                <Button>Go back</Button>
+            </Link>
+        );
+    };
+    console.log(location.pathname);
     return (
         <AppBar position="static">
             <StyledToolbar>
-                <Button color="inherit">Login</Button>
+                {nav_title !== 'portfolio' ? goBack() : ''}
+                {nav_title}
+                <Link to={'/login'}>
+                    <Button color="inherit">{loginText}</Button>
+                </Link>
             </StyledToolbar>
         </AppBar>
     );
