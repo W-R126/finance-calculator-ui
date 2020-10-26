2f92f9a3e77020bca8a9befc8db4de884e64611a
rz-roy <rz.royzhang@gmail.com>
2020-10-26 14:09:10 +0000

    loding circle

--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -1,10 +1,11 @@
-import {Box, Button, Container, Paper, TextField, Typography} from '@material-ui/core';
+import {Backdrop, Box, Button, CircularProgress, Container, Paper, TextField, Typography} from '@material-ui/core';
 import {Formik, FormikHelpers} from 'formik';
 import React, {useState} from 'react';
 import {AuthAction} from '../../api/authAPI.types';
 import {useUserState} from '../../contexts/authContext';
 import {useAuthAPI} from '../../hooks/useAuthApi';
 import {LoginFormData} from './LoginView.types';
+import {css} from 'emotion';
 
 export const LoginView: React.FC = () => {
     // eslint-disable-next-line
@@ -84,7 +85,6 @@ export const LoginView: React.FC = () => {
                                                 helperText={errors.password && touched.password && errors.password}
                                             />
                                         </Box>
-                                        {isFetching ? 'Loading...' : ''}
                                         {errorMessage}
                                         <Box mt={3} display="flex" justifyContent="center">
                                             <Button
@@ -117,6 +117,10 @@ export const LoginView: React.FC = () => {
                     </Box>
                 </Paper>
             </Box>
+
+            <Backdrop open={isFetching} style={{zIndex: 999, color: '#fff'}}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
         </Container>
     );
 };
