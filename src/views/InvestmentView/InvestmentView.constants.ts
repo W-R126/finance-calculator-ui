828fa7c39e42363e1145b41ca39e923549cd607a
rz-roy <rz.royzhang@gmail.com>
2020-10-28 12:03:26 +0100

    Small investmentView refactor

--- src/views/InvestmentView/InvestmentView.constants.ts
+++ src/views/InvestmentView/InvestmentView.constants.ts
@@ -1,1 +1,20 @@
+import {InvestmentParameters} from '../../api/investmentsAPI.types';
+import {InvestmentCategories} from './InvestmentView.types';
+
 export const currencyUnit = '$';
+
+export const initialParameters: InvestmentParameters = {
+    initialDepositValue: 634,
+    systematicDepositValue: 119,
+    frequencyInYears: 1,
+    durationInYears: 30,
+    returnOfInvestment: 9,
+    risk: 0.14,
+};
+
+export const categories = [
+    InvestmentCategories.GOLD,
+    InvestmentCategories.BONDS,
+    InvestmentCategories.REAL_ESTATE,
+    InvestmentCategories.STOCK_MARKET,
+];
---
+++ src/views/InvestmentView/InvestmentView.types.ts
@@ -0,0 +1,6 @@
+export enum InvestmentCategories {
+    GOLD = 'Gold',
+    REAL_ESTATE = 'Real estate',
+    BONDS = 'Bonds',
+    STOCK_MARKET = 'Stock Market',
+}
